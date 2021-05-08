7ad3bdc1607e6490d3289a80e3191f92543e7ce6
Filip Tobiasz (Z6FTB) <ftb@kmd.dk>
2021-05-08 12:06:33 +0200

    Create project

---
+++ CHTG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CHTG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
