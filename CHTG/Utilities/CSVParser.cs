4dc746ad6894c323cc68ec2090271c270f1765c9
Filip Tobiasz (Z6FTB) <ftb@kmd.dk>
2021-05-08 14:41:36 +0200

    Adjust validation in csv parser

--- CHTG/Utilities/CSVParser.cs
+++ CHTG/Utilities/CSVParser.cs
@@ -27,6 +27,9 @@ namespace CHTG.Utilities
                         matrix[i][j] = Convert.ToInt32(elements[j]);
                     }
                 }
+
+                ValidateDigraf(matrix);
+                ValidateOrientation(matrix);
             }
             catch(Exception ex)
             {
@@ -42,6 +45,47 @@ namespace CHTG.Utilities
             if (lines.Length != elements.Length)
             {
                 throw new InvalidInputFileException("Podana macierz nie jest kwadratowa.");
+            }
+        }
+
+        private void ValidateDigraf(int[][] matrix)
+        {
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                if (matrix[i][i] != 0)
+                {
+                    throw new InvalidInputFileException("Podany graf posiada wierzchołki które mają krawędzie do siebie.");
+                }
+            }
+
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] != 0 && matrix[i][j] != 1)
+                    {
+                        throw new InvalidInputFileException("Podany graf posiada wierzchołki pomiędzy którymi jest więcej niż jedna droga.");
+                    }
+                }
+            }
+
+            if (matrix.Length == 0)
+            {
+                throw new InvalidInputFileException("Podany plik jest pusty");
+            }
+        }
+
+        private void ValidateOrientation(int[][] matrix)
+        {
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] == 1 && matrix[j][i] == 1)
+                    {
+                        throw new InvalidInputFileException("Podany graf nie jest zorientowany.");
+                    }
+                }
             }
         }
     }
