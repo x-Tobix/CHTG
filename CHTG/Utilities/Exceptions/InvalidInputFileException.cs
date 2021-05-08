1eb823ecaea6030c0e0f1b6411299dec5699bb1e
Filip Tobiasz (Z6FTB) <ftb@kmd.dk>
2021-05-08 13:55:37 +0200

    Add exception class for invalid input file

---
+++ CHTG/Utilities/Exceptions/InvalidInputFileException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CHTG.Exceptions.Utilities
+{
+    [Serializable]
+    public class InvalidInputFileException : Exception
+    {
+        public InvalidInputFileException() : base() { }
+        public InvalidInputFileException(string message) : base(message) { }
+        public InvalidInputFileException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidInputFileException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
