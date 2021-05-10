de64ff47e340bb5de2659024735b76df8e77db68
Filip Tobiasz (Z6FTB) <ftb@kmd.dk>
2021-05-10 11:35:21 +0200

    Add int array extension for adding modulo

---
+++ CHTG/Utilities/IntArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace CHTG.Utilities
+{
+    public static class IntArrayExtensions
+    {
+        public static void AddMod(this int[] graph, int mod)
+        {
+            graph[^1] += 1;
+            for (int i = graph.Length - 1; i > 0; i--)
+            {
+                if (graph[i] == mod + 1)
+                {
+                    graph[i] = 1;
+                    graph[i - 1]++;
+                }
+            }
+        }
+    }
+}
